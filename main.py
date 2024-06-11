bc3319ddbfe9f76c0b7f6095617b65569ecbffa1
fernando-yamaguchi <fbsyamaguchi@gmail.com>
2024-06-11 15:03:50 -0300

    Teste

---
+++ main.py
@@ -0,0 +1,34 @@
+import logging
+import logging.handlers
+import os
+
+import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    HUBSPOT_TOKEN = os.environ["HUBSPOT_TOKEN"]
+except KeyError:
+    HUBSPOT_TOKEN = "Token not available!"
+    logger.info("Token not available!")
+    raise
+
+
+if __name__ == "__main__":
+    logger.info(f"Token value: {HUBSPOT_TOKEN}")
+
+    r = requests.get('https://api.hubapi.com/crm/v3/objects/users/')
+    if r.status_code == 200:
+        data = r.json()
+        user_id = data["results"][0]["id"]
+        logger.info(f'ID do primeiro usuário: {user_id}')
